4c192a1d154386933e26f4cf6bb6460622a35b31
David Tenenbaum <github email address>
2018-06-26 22:10:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+class Driver {
+  constructor(name, dayJoined) {
+    this.name = name;
+    this.dayJoined = dayJoined;
+    this.startDate = new Date;
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return (this.startDate.getFullYear() - 1) - (parseInt(this.dayJoined.split(" ").slice(-1)[0]));
+  }
+}
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    const vertical = Math.abs(parseInt(this.beginningLocation.vertical) - parseInt(this.endingLocation.vertical))
+    const horizontal = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+    return vertical + horizontal;
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
